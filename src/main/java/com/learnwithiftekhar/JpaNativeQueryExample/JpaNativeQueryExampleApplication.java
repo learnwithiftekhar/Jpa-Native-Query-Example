8bfb5dc5ad15d9850ab2330c1144f5f532b17493
Iftekhar Hossain <learnwithiftekhar@gmail.com>
2024-08-15 11:49:39 +0600

    Initial Commit

---
+++ src/main/java/com/learnwithiftekhar/JpaNativeQueryExample/JpaNativeQueryExampleApplication.java
@@ -0,0 +1,13 @@
+package com.learnwithiftekhar.JpaNativeQueryExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaNativeQueryExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaNativeQueryExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learnwithiftekhar/JpaNativeQueryExample/JpaNativeQueryExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnwithiftekhar.JpaNativeQueryExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaNativeQueryExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
